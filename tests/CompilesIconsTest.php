ecb021529e5df70d2ca66c7f92fb06af4f87b4e1
John Carlo C Advincula <jc29advincula@gmail.com>
2024-02-18 13:52:53 +0800

    Update CompilesIconsTest.php

--- tests/CompilesIconsTest.php
+++ tests/CompilesIconsTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Tests;
 
-use BladeUI\Heroicons\BladeHeroiconsServiceProvider;
+use JohnCarlo29\Cartzilla\BladeCartzillaServiceProvider;
 use BladeUI\Icons\BladeIconsServiceProvider;
 use Orchestra\Testbench\TestCase;
 
@@ -57,7 +57,7 @@ class CompilesIconsTest extends TestCase
     {
         return [
             BladeIconsServiceProvider::class,
-            BladeHeroiconsServiceProvider::class,
+            BladeCartzillaServiceProvider::class,
         ];
     }
 }
